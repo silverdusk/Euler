3b3cf0887f97d4a6e185807b39d8a421b86dfa15
silverdusk <kiryxa@gmail.com>
2022-01-07 13:26:11 +0200

    Euler_1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
-    public static void main() {
-
+    public static void main(String[] args) {
+        int resultOne = new Euler_1().EulerOne(1, 1000);
+        System.out.println("The answer is: " + resultOne);
     }
 }
